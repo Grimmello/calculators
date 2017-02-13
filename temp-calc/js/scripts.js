16833c4c98d22c8fdbc629a5b4363ebb47dfb2fe
Deric Miguel <deric.miguel@gmail.com>
2017-02-13 15:26:05 -0800

    tetsing

--- temp-calc/js/scripts.js
+++ temp-calc/js/scripts.js
@@ -1,16 +1,29 @@
-
-var converter = function (tconvert) {
-  alert("your temperature converted is " + tconvert +" degrees.")
-}
+var tempF = null
+var tempC = null
 var specify = prompt("Enter C to convert Fahrenheit to Celsius or F to convert Celsius to Fahrenheit.");
 
-if(specify === "C") {var tempC = parseInt(prompt("Enter the Fahrenheit temperature.")), conversionF = 0} else if (specify === "F") {var tempF = parseInt(prompt("Enter the Celsius temperature."))};
-var conversionC = (((tempC - 32) * 5) / 9);
-var conversionF = (((tempF * 9) / 5) + 32);
 if(specify === "C") {
-  var conversionF = 0}
+   tempC = parseInt(prompt("Enter the Fahrenheit temperature.")), conversionF = 0
+   tempF = 0
+} 
 else if (specify === "F") {
-  var conversionC = 0
+   tempF = parseInt(prompt("Enter the Celsius temperature."))
+   tempC  = 0
 }
 
-converter(conversionC + conversionF)
+converter(conversionC(tempC) + conversionF(tempF)
+function converter (tconvert) }
+  alert("your temperature converted is " + tconvert +" degrees.")
+}
+function  conversionC (tempC) {
+   x = tempC - 32  
+   return (x * 5) / 9
+} 
+
+function conversionF (tempF) {
+   x = (tempF * 9) / 5 
+   return x + 32
+}
+
+
+
