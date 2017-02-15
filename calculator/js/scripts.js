3886b752f4735f6b2ec21d3f60000a38c4b8d393
Clifford Grimmell and Jahan Walsh <jahan@>
2017-02-15 10:56:55 -0800

    add subtract, multiply, divide functions as seperate forms in page, each work individually

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -1,21 +1,52 @@
-var add = function(number1, number2) {
+number1 = 0;
+number2 = 0;
+function add (number1, number2) {
 	return number1 + number2;
 };
-var subtract = function(number1, number2) {
+
+function subtract(number1, number2) {
 	return number1 - number2;
 };
-var multiply = function(number1, number2) {
+
+function multiply(number1, number2) {
 	return number1 * number2;
 };
-var division = function(number1, number2) {
+
+function divide(number1, number2) {
 	return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter a number:"));
-var sum = add(number1, number2);
-var difference = subtract(number1, number2);
-var product = multiply(number1, number2);
-var quotient = division(number1, number2);
+// Below is Front End logic
+$(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1,number2);
+		$("#output1").text(result);
+	});
 
-alert();
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1,number2);
+		$("#output2").text(result);
+	});
+
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1,number2);
+		$("#output3").text(result);
+	});
+
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1,number2);
+		$("#output4").text(result);
+	});
+});
